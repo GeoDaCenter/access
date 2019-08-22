9560458b61dd525d05afefb8e1a4b6252bd6ad66
yatlas <atlascube@gmail.com>
2019-08-22 17:37:07 -0500

    changed lambda zip maker

--- meta/lambda/createLambdaZip.sh
+++ meta/lambda/createLambdaZip.sh
@@ -10,9 +10,9 @@ rm -r *.dist-info __pycache__
 
 rm -r pandas numpy *.dist-info
 
-wget https://files.pythonhosted.org/packages/1d/9a/7eb9952f4b4d73fbd75ad1d5d6112f407e695957444cb695cbb3cdab918a/pandas-0.25.0-cp36-cp36m-manylinux1_x86_64.whl
+wget https://files.pythonhosted.org/packages/e6/de/a0d3defd8f338eaf53ef716e40ef6d6c277c35d50e09b586e170169cdf0d/pandas-0.24.1-cp36-cp36m-manylinux1_x86_64.whl
 
-wget https://files.pythonhosted.org/packages/19/b9/bda9781f0a74b90ebd2e046fde1196182900bd4a8e1ea503d3ffebc50e7c/numpy-1.17.0-cp36-cp36m-manylinux1_x86_64.whl
+wget https://files.pythonhosted.org/packages/f5/bf/4981bcbee43934f0adb8f764a1e70ab0ee5a448f6505bd04a87a2fda2a8b/numpy-1.16.1-cp36-cp36m-manylinux1_x86_64.whl
 
 unzip numpy-1.17.0-cp36-cp36m-manylinux1_x86_64.whl
 

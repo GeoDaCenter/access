6e2a851e5320b76058073a8d8802f399a86bac2f
Jamie Saxon <jsaxon@uchicago.edu>
2019-09-22 17:15:23 -0500

    way cleaner script for making the lambda directory.  don't get a specific file, just pip install locally and delete the dist and caches also, delete the .git directory to keep under the 50 mb limit for aws lambda

--- meta/lambda/createLambdaZip.sh
+++ meta/lambda/createLambdaZip.sh
@@ -2,24 +2,20 @@
 
 cd lambdadata
 
-git clone https://github.com/JamesSaxon/access.git
-
-pip3 install -t . pandas
-
+rm -rf access/ numpy/ pandas/ pytz/ six.py dateutil/ bin/
 rm -r *.dist-info __pycache__
 
-rm -r pandas numpy *.dist-info
+git clone https://github.com/JamesSaxon/access.git
+rm -rf access/.git
 
-wget https://files.pythonhosted.org/packages/e6/de/a0d3defd8f338eaf53ef716e40ef6d6c277c35d50e09b586e170169cdf0d/pandas-0.25.1-cp37-cp37m-manylinux1_x86_64.whl
+# NB that you should do this in a python 3.7 environment, 
+# or at any rate, one that matches the one used for the AWS fn
 
-wget https://files.pythonhosted.org/packages/f5/bf/4981bcbee43934f0adb8f764a1e70ab0ee5a448f6505bd04a87a2fda2a8b/numpy-1.17.0-cp37-cp37m-manylinux1_x86_64.whl
-
-unzip numpy-1.17.0-cp37-cp37m-manylinux1_x86_64.whl
-
-unzip pandas-0.25.1-cp37-cp37m-manylinux1_x86_64.whl
-
-pip3 install -t . pytz
-
-rm -r *.whl *.dist-info __pycache__
+pip install -t . pandas
+rm -r *.dist-info __pycache__
 
 zip -r zip.zip .
+mv zip.zip ../
+
+cd ../
+
